afadeef21ab31eabb7b48359633aa046a75a5ae8
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-04 17:26:56 -0500

    webpack configurations

---
+++ react-and-patterns-performance/webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const config = {
+  devServer: {
+    compress: true,
+    hot: true,
+    port: 8000,
+    historyApiFallback: true,
+    open: true,
+    watchOptions: {
+      ignored: /node_modules/
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-syntax-dynamic-import']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
+
+module.exports = config
---
+++ react-and-patterns-performance/webpack.optimized.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge')
+const configuration = require('./webpack.config')
+const optimization = {
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: 'commons',
+          chunks: 'initial',
+          minChunks: 2
+        },
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all'
+        }
+      }
+    }
+  }
+}
+module.exports = merge(optimization, configuration)
