94d1ca2cf85f77ab9cb5ba4138841dda0a7cdeed
DezkaReid <elmaildeldezkareid@gmail.com>
2018-10-04 17:27:55 -0500

    entrypoints

---
+++ react-and-patterns-performance/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Demo from './samples/async-components'
+
+const appElement = document.getElementById('app-element')
+
+render(<Demo />, appElement)
