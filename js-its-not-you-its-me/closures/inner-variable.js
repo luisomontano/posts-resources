2dc096f685a2c23ab6e96f4fbb8a03e8fcdefe50
DezkaReid <elmaildeldezkareid@gmail.com>
2019-01-22 20:06:17 -0600

    printing global variable and local variable

---
+++ js-its-not-you-its-me/closures/inner-variable.js
@@ -0,0 +1,12 @@
+function papaluchon () {
+  function huerco () {
+    const myCheve = 'Corona'
+    console.log(cheve, myCheve)
+  }
+  cheve = 'Tecate'
+  return huerco
+}
+
+const huerco = papaluchon()
+huerco()
+console.log(cheve)
