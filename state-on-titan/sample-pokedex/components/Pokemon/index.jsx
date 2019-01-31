caa3e530eb1eb7e6a7af6f08beb83752ac1c113a
DezkaReid <elmaildeldezkareid@gmail.com>
2019-01-30 20:36:18 -0600

    pokemon component

---
+++ state-on-titan/sample-pokedex/components/Pokemon/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Pokemon (props) {
+  return (
+    <article>
+      <p>{props.name}</p>
+    </article>
+  )
+}
+
+export default Pokemon
---
+++ state-on-titan/sample-pokedex/components/Pokemon/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+function Pokemon (props: any) {
+  return (
+    <article>
+      <p>{props.name}</p>
+    </article>
+  )
+}
+
+export default Pokemon
