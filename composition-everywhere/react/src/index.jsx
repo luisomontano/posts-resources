c254492351b3cf00029b592e15463f529328317b
DezkaReid <elmaildeldezkareid@gmail.com>
2019-02-22 16:49:26 -0600

    main js file

---
+++ composition-everywhere/react/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { render } from 'react-dom'
+import WrapperMap from './WrapperMap.jsx'
+
+const appElement = document.getElementById('map')
+
+render(<WrapperMap />, appElement)
