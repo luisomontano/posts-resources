3954f1545a202f5cea1822c046b91800503d388d
DezkaReid <elmaildeldezkareid@gmail.com>
2019-02-28 17:38:54 -0600

    fixed hook

--- hooks/__tests__/EffectHook-test.js
+++ hooks/__tests__/EffectHook-test.js
@@ -8,13 +8,13 @@ describe('effect hook suite', () => {
 
   test('Hook first paint', () => {
     const { getByText } = render(<EffectHook />)
-    expect(getByText(/Updating With Mount times 2/i)).not.toBeNull()
+    expect(getByText(/Message changes 1/i)).not.toBeNull()
   })
 
   test('Hook increment counter updating property', () => {
     const { getByText, rerender } = render(<EffectHook message='Up to' />)
-    expect(getByText(/Up to 2/i)).not.toBeNull()
+    expect(getByText(/Up to 1/i)).not.toBeNull()
     rerender(<EffectHook />)
-    expect(getByText(/Updating With Mount times 5/i)).not.toBeNull()
+    expect(getByText(/Message changes 2/i)).not.toBeNull()
   })
 })
--- hooks/samples/EffectHook.jsx
+++ hooks/samples/EffectHook.jsx
@@ -1,14 +1,11 @@
 import React, { useEffect, useState } from 'react'
 
-function EffectHook ({ message = 'Updating With Mount times' }) {
+function EffectHook ({ message = 'Message changes' }) {
   const [countUpdateWithMount, setCount] = useState(0)
 
   useEffect(() => {
     setCount(countUpdateWithMount + 1)
-    return () => {
-      setCount(0)
-    }
-  })
+  }, [message])
 
   return (
     <div>
