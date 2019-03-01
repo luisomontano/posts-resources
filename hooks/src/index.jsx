b9eac4247fdfcdb264514238cc969da413862230
DezkaReid <elmaildeldezkareid@gmail.com>
2019-03-01 02:54:11 -0600

    added entrypoint

---
+++ hooks/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './components/App.jsx'
+
+const appElement = document.getElementById('app')
+render(<App />, appElement)
